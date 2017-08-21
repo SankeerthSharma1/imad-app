510e01f0a2da09b13d35b1f87eea98c1a5ec098d
SankeerthSharma1 <30749303+SankeerthSharma1@users.noreply.github.com>
2017-08-21 20:16:47 +0530

    articlename param added to server.js

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+	comments = [];
+  	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/temp.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'temp.js'));
 });
 
 var articles = {
@@ -28,7 +37,7 @@ var articles = {
 		title:'Article Three Page',
 		heading:'Article Three',
 		date: '25, Aug 2017',
-		content: '<p>Third article content goes 555 here. </p>'
+		content: '<p>Third article content goes here. </p>'
 	}
 };
 
@@ -37,13 +46,15 @@ function createTemplate(data) {
 	var date = data.date;
 	var heading = data.heading;
 	var content = data.content;
-	var htmlTemplate=`<html>
+	var htmlTemplate=`<!doctype html>
+		<html>
 		<head>
-			<title>${title}</title>
-			<link rel="stylesheet" type="text/css" href="/ui/style.css">
+			<title>${title}</title>			
 			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" type="text/css" href="/ui/style.css"/>
 		</head>
 		<body>
+		  <script type="text/javascript" src="../ui/temp.js"></script>
 		<div>
 			<a href="/">home</a>
 		</div>
@@ -54,12 +65,43 @@ function createTemplate(data) {
 		</div>
 		<div class='container'>
 			${content}
+
+			Comment <!--<input type="text" id="comment" placeholder="enter comment"></input>-->
+
+			<textarea id=comment rows="4" cols="50"></textarea>
+			<input type="submit" id="btnComment" onclick="postComment();"></input>
 		</div> 
+			<div class="center"><ul id="commentList">  			
+			</ul></div>
 		</body>
 		</html>`;
 
 		return htmlTemplate;
 }
+
+var counter=0;
+app.get('/counter', function(req, res) {
+	counter++;
+	res.send(counter.toString());
+});
+
+var names = [];
+//app.get('/submitName/:name', function (req, res) {
+app.get('/submitName', function (req, res) {
+	//var name=req.params.name;
+	var name=req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
+
+var comments = [];
+//app.get('/submitName/:name', function (req, res) {
+app.get('/submitComment', function (req, res) {
+	//var name=req.params.name;
+	var comment=req.query.comment;
+	comments.push(comment);
+	res.send(JSON.stringify(comments));
+});
 
 app.get('/:articleName', function (req, res) {
   //res.send('article one requested will be served shortly');
@@ -75,7 +117,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
