a11555ed56101c0159e3841bcc8b75faad0df602
SankeerthSharma1 <30749303+SankeerthSharma1@users.noreply.github.com>
2017-08-21 20:17:41 +0530

    changes made to js and html files committed

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,72 @@
 console.log('Loaded!');
+//alert("eree");
+
+window.onload = function() {
+	//alert("eree");
+};
+
+var counter=0;
+var button = document.getElementById("btnCounter");
+//alert(btnCtr.id);
+
+button.onclick=function(){
+	//alert(counter);
+	/*var span = document.getElementById("spanCounter");
+	counter++;
+	span.innerHTML = counter.toString();*/
+
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status===200) {
+				var ctr=request.responseText;
+				var span = document.getElementById("spanCounter");
+				span.innerHTML = ctr.toString();
+			}
+		}
+	};
+
+	request.open('GET','http://localhost/counter',true);
+	request.send(null);
+};
+
+var buttonSubmit = document.getElementById("btnSubmit");
+//alert(buttonSubmit.id);
+
+
+buttonSubmit.onclick=function(){
+	//alert("abcd");
+
+	var elmntName = document.getElementById("name");
+	var nameVal = elmntName.value;
+
+	var ulList = document.getElementById("nameList");
+
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status===200) {
+				var strNames="";
+				//var names = ["name1", "name2", "name3"];
+				var names=request.responseText;
+				names = JSON.parse(names);
+				for (var i=0;i<names.length;i++) {
+					strNames += "<li>"+names[i]+"</li>";
+				}
+				
+				//alert(strNames);
+				ulList.innerHTML = strNames;
+			}
+		}
+	};
+
+	request.open('GET','http://localhost/submitName?name='+nameVal,true);
+	request.send(null);	
+};
+
+var buttonComment = document.getElementById("btnComment");
+alert(buttonComment.id);
+
+buttonComment.onclick=function(){
+	alert("abcd");
+};
--- ui/temp.js
+++ ui/temp.js
@@ -1,1 +1,34 @@
+var buttonComment = document.getElementById("btnComment");
+//alert(buttonComment.id);
 
+/*buttonComment.onclick=function(){
+	alert("abcd");
+};*/
+
+function postComment() {	
+	var elmntComment= document.getElementById("comment");
+	var commentVal = elmntComment.value;
+	//alert(commentVal);
+	var commentList = document.getElementById("commentList");
+
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status===200) {
+				var strComments="";
+				//var names = ["name1", "name2", "name3"];
+				var comments=request.responseText;
+				comments = JSON.parse(comments);
+				for (var i=0;i<comments.length;i++) {
+					strComments += "<li>"+comments[i]+"</li>";
+				}
+				
+				//alert(strComments);
+				commentList.innerHTML = strComments;
+			}
+		}
+	};
+
+	request.open('GET','http://localhost/submitComment?comment='+commentVal,true);
+	request.send(null);	
+}
