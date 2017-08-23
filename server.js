ac1bc081974585160696d7619de63cc9b89f52e4
SankeerthSharma1 <30749303+SankeerthSharma1@users.noreply.github.com>
2017-08-23 13:28:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,6 +131,23 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('article/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+    pool.query("select title,heading,date,content from author where title='"+req.params.articleName+"'", function(err, result) {
+		if(err) {
+			res.status(500).send(err.toString());
+		} else {
+		    if(result.rows.length == 0) {
+			    res.status(404).send("No data found");
+		    } else {
+		        //res.send(JSON.stringify(result.rows));
+		        var articleData = result.rows[0];
+		        res.send(createTemplate(articleData));
+		    }
+		}
+	});
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
